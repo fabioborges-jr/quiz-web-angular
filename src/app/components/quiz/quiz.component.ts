21a4267ed7d3ec5651f77670ba231ed38fff51ba
Fabio <fabioborgesouzajunior@gmail.com>
2024-02-14 19:33:11 -0300

    :sparkles: feat: Adds question property to quiz.component

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -9,7 +9,10 @@ import {questions, results, title} from "../../../assets/data/quizz_questions.js
   styleUrl: './quiz.component.scss'
 })
 export class QuizComponent implements OnInit{
-
+  questionIndex:number=0
+  question?:string
+  
   ngOnInit(): void {
+    this.question=questions[this.questionIndex].question
   }
 }
