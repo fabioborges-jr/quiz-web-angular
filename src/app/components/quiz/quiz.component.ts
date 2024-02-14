8582e9ceb801144dfacad89e6555f50fa4e2ffcb
Fabio <fabioborgesouzajunior@gmail.com>
2024-02-14 19:54:49 -0300

    :sparkles: feat: Adds first question options

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import {questions, results, title} from "../../../assets/data/quizz_questions.json"
+import {questions, results} from "../../../assets/data/quizz_questions.json"
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'quiz',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './quiz.component.html',
   styleUrl: './quiz.component.scss'
 })
 export class QuizComponent implements OnInit{
   questionIndex:number=0
   question?:string
+  options?: {
+    id: number
+    name: string
+    alias: string
+  }[]
   
   ngOnInit(): void {
     this.question=questions[this.questionIndex].question
+    this.options=questions[this.questionIndex].options
   }
 }
