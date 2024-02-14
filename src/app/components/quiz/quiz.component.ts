ba8038d3986348f73738fa5c2c15cefadf343d48
Fabio <fabioborgesouzajunior@gmail.com>
2024-02-14 19:16:49 -0300

    :boom: fix: Removes title property from quiz.component

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -9,9 +9,7 @@ import {questions, results, title} from "../../../assets/data/quizz_questions.js
   styleUrl: './quiz.component.scss'
 })
 export class QuizComponent implements OnInit{
-  title?:string
 
   ngOnInit(): void {
-    this.title=title
   }
 }
